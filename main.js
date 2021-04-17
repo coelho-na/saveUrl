56d534a35a6f156cd16ac38822518e84c0ccc81b
Nadine Coelho <nadinecoelho9@gmail.com>
2021-04-17 15:58:20 -0300

    update

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ let sectionPost = document.querySelector(".urls__post");
 let inputImage = document.querySelector("input#image");
 let inputVideo = document.querySelector("input#video");
 let inputLink = document.querySelector("input#link");
+let div = document.querySelector("#linkDiv");
+div.style.display = "none";
 
 // Event When Click on Button
 btn.addEventListener("click", createUrlList);
@@ -13,27 +15,42 @@ btn.addEventListener("click", createUrlList);
 // function to fire when btn is clicked
 
 function createUrlList() {
+  let random = Math.floor(Math.random() * (50 - 20) + 25);
+  let newList = document.createElement("div");
+  newList.classList.add("urls__content");
+
   if (inputImage.checked && url.value != "") {
-    sectionPost.innerHTML += `
-    <div >
-      <img src="${url.value}" />
-      <i class="fas fa-times button__close"></i>
-    </div>`;
+    sectionPost.appendChild(newList);
+    newList.setAttribute("id", random);
+    newList.innerHTML += `
+        <img class="urls__content--img" src="${url.value}" />
+        <span class="button__close"><i class="fas fa-times urls__icon"></i></span>
+      `;
   }
   if (inputVideo.checked && url.value != "") {
-    sectionPost.innerHTML += `
-    <div>
-      <iframe width="100%" height="480" src="${url.value}">
-      </iframe>
-     <i class="fas fa-times button__close"></i> 
-    </div>`;
+    sectionPost.appendChild(newList);
+    newList.setAttribute("id", random);
+    newList.innerHTML += `
+    <iframe class="urls__content--video" width="100%" height="480" src="${url.value}">
+    </iframe>
+    <a class="button__close"><i class="fas fa-times urls__icon"></i></a>
+  `;
   }
   if (inputLink.checked && url.value != "") {
-    sectionPost.innerHTML += `
-    <div >
-      <a href="${url.value}" target="_blank">${url.value}</a>
-      <i class="fas fa-times button__close" ></i>
-    </div>`;
+    div.style.display = "block";
+    let link = document.querySelector("#linkDiv a:nth-child(1)");
+    link.setAttribute("href", url.value);
+    link.textContent = url.value;
+
+    sectionPost.appendChild(div);
+    div.innerHTML += `
+    <a class="urls__content--link" href="${url.value}" target="_blank">${url.value}</a>
+    <a class="button__close"><i class="fas fa-times urls__icon "></i></a>
+    `;
+    // // let link = `
+    // //  <a class="urls__content--link" href="${url.value}" target="_blank">${url.value}</a>
+    // //  <a class="button__close"><i class="fas fa-times urls__icon "></i></a>
+    // //  `;
   }
 
   if (url.value == "") {
@@ -55,10 +72,10 @@ function createUrlList() {
 }
 
 //Delete div
-
-document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("button__close")) {
-    let chield = document.querySelector(".urls__post  div");
-    chield.remove();
-  }
+let btns = document.querySelectorAll(".urls__content .urls__icon");
+Array.from(btns).forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    const divToExclude = e.target.parentElement.parentElement;
+    divToExclude.parentNode.removeChild(divToExclude);
+  });
 });
